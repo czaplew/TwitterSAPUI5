aa261d981f13c19b128ffdb17569a4a832f06de2
S0015565168 <kcz@gavdi.com>
2018-06-03 10:35:04 +0000

    Working build

---
+++ webapp/controller/Basic.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function(Controller) {
+	"use strict";
+
+	return Controller.extend("medium.News.controller.Basic", {
+
+		arrow: function() {
+			const xx = 2;
+			
+		},
+
+	});
+});
